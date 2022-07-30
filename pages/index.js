17d17b6bb65085f892e49c94cd417dca443a07b7
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2022-07-30 15:50:12 +0200

    Styling the footer

--- pages/index.js
+++ pages/index.js
@@ -162,7 +162,7 @@ export default function Home({ subscription }) {
         Copyright 2022 <b> <a href='https://ayivugwe.editorx.io/ayivugwekabemba'>Ayivugwe Kabemba.</a></b>  All rights reserved.
           </div>
           <div className="max-w-screen-sm mx-auto text-center">
-        Niliri ku <span className="text-red-500 text-xs"><a href="https://www.instagram.com/Ibufuliiru/">Instagram</a></span>
+        Niliri ku <a className='text-red-500 txt-xs' href="https://www.instagram.com/Ibufuliiru/">Instagram</a>
                       na ku  <a className='text-red-500 txt-xs' href='https://twitter.com/AyivugweKabemba'>Twitter</a> na ku  <a className='text-red-500 txt-xs' href='https://www.facebook.com/Ibufuliiru/'>Facebook</a>
         </div>
           <div className="max-w-screen-sm mx-auto text-center">
