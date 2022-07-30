9ae91bcf5b64e63db50bc2dc0b46761af6139ddc
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2022-07-30 15:34:23 +0200

    Still changing the styles

--- pages/index.js
+++ pages/index.js
@@ -159,11 +159,11 @@ export default function Home({ subscription }) {
       </div>
       <footer>
         <div className="max-w-screen-sm mx-auto text-center">
-        Copyright 2022 <b> <a color='red' href='https://ayivugwe.editorx.io/ayivugwekabemba'>Ayivugwe Kabemba.</a></b>  All rights reserved.
+        Copyright 2022 <b> <a href='https://ayivugwe.editorx.io/ayivugwekabemba'>Ayivugwe Kabemba.</a></b>  All rights reserved.
           </div>
           <div className="max-w-screen-sm mx-auto text-center">
-        Niliri ku <span className="text-gray-500 text-xs"><a href="https://www.instagram.com/Ibufuliiru/">Instagram</a></span>
-                      na ku  <a href='https://twitter.com/AyivugweKabemba'>Twitter</a> na ku  <a href='https://www.facebook.com/Ibufuliiru/'>Facebook</a>
+        Niliri ku <span className="text-red-500 text-xs"><a href="https://www.instagram.com/Ibufuliiru/">Instagram</a></span>
+                      na ku  <a className='text-red-500 txt-xs' href='https://twitter.com/AyivugweKabemba'>Twitter</a> na ku  <a href='https://www.facebook.com/Ibufuliiru/'>Facebook</a>
         </div>
           <div className="max-w-screen-sm mx-auto text-center">
             Developed and Deployed using DatoCMS and Vercel in TypeScript.
