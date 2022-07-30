7a87da83e1cb3bc6fb2b7616ec7b959685d893f6
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2022-07-30 15:46:45 +0200

    The social media links in the footer are in red

--- pages/index.js
+++ pages/index.js
@@ -163,7 +163,7 @@ export default function Home({ subscription }) {
           </div>
           <div className="max-w-screen-sm mx-auto text-center">
         Niliri ku <span className="text-red-500 text-xs"><a href="https://www.instagram.com/Ibufuliiru/">Instagram</a></span>
-                      na ku  <a className='text-red-500 txt-xs' href='https://twitter.com/AyivugweKabemba'>Twitter</a> na ku  <a href='https://www.facebook.com/Ibufuliiru/'>Facebook</a>
+                      na ku  <a className='text-red-500 txt-xs' href='https://twitter.com/AyivugweKabemba'>Twitter</a> na ku  <a className='text-red-500 txt-xs' href='https://www.facebook.com/Ibufuliiru/'>Facebook</a>
         </div>
           <div className="max-w-screen-sm mx-auto text-center">
             Developed and Deployed using DatoCMS and Vercel in TypeScript.
