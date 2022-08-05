96b84136b827278f9f6f78cd4a02bc95d89b7c0f
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2022-08-05 10:31:35 +0200

    I added some menu items

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 const Navbar = () =>{
     return (
-        <div>Navbar</div>
+        <div>
+            <ul>
+                <Link>Ndondeezo</Link>
+                <Link>Twehe</Link>
+                <Link>Kifuliiru</Link>
+                <Link>Ibufuliiru</Link>
+                <Link>Bafuliiru</Link>
+                <Link>Guno mukolwa</Link>
+                <Link>Tuyandikire</Link>
+                <Link>Imyazi</Link>
+
+            </ul>
+                </div>
     ) ;
 }
 
