9557dc962a3be9818ab380a21dcf269112feb94e
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2022-08-05 10:38:13 +0200

    I added some other pages

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,19 +2,19 @@ import React from 'react';
 
 const Navbar = () =>{
     return (
-        <div>
+        <nav>
             <ul>
-                <Link>Ndondeezo</Link>
-                <Link>Twehe</Link>
-                <Link>Kifuliiru</Link>
-                <Link>Ibufuliiru</Link>
-                <Link>Bafuliiru</Link>
-                <Link>Guno mukolwa</Link>
-                <Link>Tuyandikire</Link>
-                <Link>Imyazi</Link>
+                <Link href='/'> <a><li>Ndondeezo></li></a></Link>
+                <Link href='/'><a><li>Twehe</li></a></Link>
+                <Link href='/'><a><li>Kifuliiru</li></a></Link>
+                <Link href='/'><a><li>Ibufuliiru</li></a></Link>
+                <Link href='/'><a><li>Bafuliiru</li></a></Link>
+                <Link href='/'><a><li>Guno mukolwa</li></a></Link>
+                <Link href='/'><a><li>Tuyandikire</li></a></Link>
+                <Link href='/'><a><li>Imyazi</li></a></Link>
 
             </ul>
-                </div>
+        </nav>
     ) ;
 }
 
