ca3f8df2e5eea5755cd9102ba50df789956845ba
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2022-08-05 10:51:25 +0200

    Some changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,19 +3,7 @@ import React from 'react';
 const Navbar = () =>{
     return (
         <div>Navbar</div>
-        <nav>
-            {/* <ul>
-                <Link href='/'> <a><li>Ndondeezo></li></a></Link>
-                <Link href='/'><a><li>Twehe</li></a></Link>
-                <Link href='/'><a><li>Kifuliiru</li></a></Link>
-                <Link href='/'><a><li>Ibufuliiru</li></a></Link>
-                <Link href='/'><a><li>Bafuliiru</li></a></Link>
-                <Link href='/'><a><li>Guno mukolwa</li></a></Link>
-                <Link href='/'><a><li>Tuyandikire</li></a></Link>
-                <Link href='/'><a><li>Imyazi</li></a></Link>
-
-            </ul> */}
-        </nav>
+        
     ) ;
 }
 
