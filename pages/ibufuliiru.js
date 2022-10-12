adb11fca9b79c28e80f38db99e74569d1d12c5e8
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2022-10-12 12:59:52 +0200

    Added Ibufuliiru and Twehe pages

---
+++ pages/ibufuliiru.js
@@ -0,0 +1,5 @@
+export default function Ibufuliiru(){
+    return (
+        <div>Ibufuliiru</div>
+    );
+}
---
+++ pages/twehe.js
@@ -0,0 +1,5 @@
+export default function Twehe(){
+    return (
+        <div>Twehe</div>
+    );
+}
