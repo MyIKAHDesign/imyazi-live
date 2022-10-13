6ace00bea459356db79f79de2defe58cf822f935
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2022-10-13 13:17:03 +0200

    I changed the Ibufuliiru page

--- pages/ibufuliiru.js
+++ pages/ibufuliiru.js
@@ -1,5 +1,130 @@
 export default function Ibufuliiru(){
     return (
-        <div>Ibufuliiru</div>
+        <div className="text-gray-700 body-font py-12 bg-gray-100 px-10">
+     
+      <Head>
+        <title>Tulonge imyazi ye'Bufuliiru mu Kifuliiru. Menya ibimudetwa ku bufuliiru</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Tulonge imyazi ye'Bufuliiru mu Kifuliiru. Menya ibimudetwa ku bufuliiru. Tulonge imyazi ye Kifuliiru, Bafuliiru, Ibufuliiru na hoshi na byooshi ikiri miduutu miduutu mu Kifuliiru" />
+        <meta name="description" content="Tulonge imyazi ye'Bufuliiru mu Kifuliiru. Menya ibimudetwa ku bufuliiru. Kifuliiru. Tulonge imyazi ye Kifuliiru, Bafuliiru, Ibufuliiru na hoshi na byooshi ikiri miduutu miduutu mu Kifuliiru" />
+        
+      </Head>
+
+      <div className="max-w-screen-sm mx-auto text-center">
+        <h3 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-5xl sm:leading-10">
+          Ibufuliiru mu Kifuliiru
+       </h3>
+      </div>
+
+      <br/>
+
+      <div className="max-w-screen-sm mx-auto text-center">
+       <p><a className='text-red-500 txt-xs' href='https://ibufuliiru.editorx.io/ibufuliiru/' target="_blank">Guno mukolwa</a> | <a className='text-red-500 txt-xs' href='https://ibufuliiru.editorx.io/ibufuliiru/' target="_blank">Kifuliiru</a> | <a className='text-red-500 txt-xs' href='https://ibufuliiru.editorx.io/ibufuliiru/' target="_blank">Bafuliiru</a> | <a className='text-red-500 txt-xs' href='https://ibufuliiru.editorx.io/ibufuliiru/' target="_blank">Ibufuliiru</a> | <a className='text-red-500 txt-xs' href='https://ibufuliiru.editorx.io/ibufuliiru/' target="_blank">Imyazi mu Kifuliiru</a></p>
+      </div>
+      <br/>
+
+      <div className="max-w-screen-sm mx-auto text-center">
+ 
+        <p className="text-base leading-6 text-indigo-600 font-semibold tracking-wide uppercase">
+        Tulonge imyazi ye'Bufuliiru mu Kifuliiru. Menya ibimudetwa ku bufuliiru mu Kifuliiru.
+        </p>    
+      </div>
+
+      <div className="max-w-screen-sm mx-auto text-center mt-20 mb-12">
+        {status === 'connecting' ? (
+          <div>Tugweti tugalooza imyazi...</div>
+        ) : status === 'connected' ? (
+          <div className="flex flex-col md:flex-row items-center justify-center">
+            <span className="flex h-3 w-3 relative mb-3 md:mb-0 md:mr-2">
+              <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-pink-400 opacity-75"></span>
+              <span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500"></span>
+            </span>
+            <span>Ino myazi yooshi mubwini hano hofi yo mihyahya tugweti!</span>
+          </div>
+        ) : (
+          <div>Twayuusa</div>
+        )}
+      </div>
+
+      {error && (
+        <div className="max-w-screen-sm my-12 mx-auto">
+          <h1 className="title-font text-lg font-bold text-gray-900 mb-3">
+            Error: {error.code}
+          </h1>
+          <div className="my-5">{error.message}</div>
+          {error.response && (
+            <pre className="bg-gray-100 p-5 mt-5 font-mono">
+              {JSON.stringify(error.response, null, 2)}
+            </pre>
+          )}
+        </div>
+      )}
+
+      <div className="max-w-screen-sm mx-auto my-12">
+        {data && (
+          <TransitionGroup>
+            {data.posts.map((post) => (
+              <CSSTransition
+                key={post.id}
+                classNames={{
+                  enter: 'post-enter',
+                  enterActive: 'post-enter-active',
+                  exit: 'post-exit',
+                  exitActive: 'post-exit-active',
+                }}
+                timeout={{ enter: 1200, exit: 1200 }}
+              >
+                <div>
+                  <div className="shadow-xl rounded-lg overflow-hidden bg-white">
+                    {post.photos.map((photo) => (
+                      <Image
+                        key={photo.responsiveImage.src}
+                        className="w-full"
+                        data={photo.responsiveImage}
+                      />
+                    ))}
+                    {post.content && (
+                      <div className="p-4 md:p-8 md:text-xl content">
+                        <ReactMarkdown children={post.content} />
+                      </div>
+                    )}
+                  </div>
+                  <div className="mt-4 grid grid-cols-2 text-xs md:text-sm text-gray-500 md:px-8 items-center pb-12">
+                    <div className="flex items-center">
+                      <div className="w-8 h-8 relative">
+                        <Image
+                          className="rounded-full mr-2 shadow"
+                          layout="fill"
+                          data={post.author.avatar.responsiveImage}
+                        />
+                      </div>
+                      <div className="pl-2">{post.author.name}</div>
+                    </div>
+                    <div className="text-right">
+                      <TimeAgo date={post._firstPublishedAt} />
+                    </div>
+                  </div>
+                </div>
+              </CSSTransition>
+            ))}
+          </TransitionGroup>
+        )}
+      </div>
+      <footer>
+        <div className="max-w-screen-sm mx-auto text-center">
+        Copyright 2022 <b> <a href='https://ayivugwe.editorx.io/ayivugwekabemba'>Ayivugwe Kabemba.</a></b>  All rights reserved.
+          </div>
+          <div className="max-w-screen-sm mx-auto text-center">
+        Niliri ku <a className='text-red-500 txt-xs' href="https://www.instagram.com/Ibufuliiru/">Instagram</a> na ku  <a className='text-red-500 txt-xs' href='https://twitter.com/AyivugweKabemba'>Twitter</a> na ku  <a className='text-red-500 txt-xs' href='https://www.facebook.com/Ibufuliiru/'>Facebook</a> 
+        </div>
+          <div className="max-w-screen-sm mx-auto text-center">
+            Developed and Deployed using DatoCMS and Vercel in Next.js.
+          </div>
+      </footer>
+    </div>
+  
     );
 }
+
+
+
