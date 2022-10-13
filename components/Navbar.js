a3a5724de7ca160475cf6a321a7c26c6fd4c8512
MyIKAHDesign <99662370+MyIKAHDesign@users.noreply.github.com>
2022-10-13 14:36:49 +0200

    Changing to bring back the website

--- components/Navbar.js
+++ components/Navbar.js
@@ -60,25 +60,25 @@ const Navbar = () => {
   </div>
   <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
     <div class="text-sm lg:flex-grow">
-      <Link href={"/ibufuliiru"} class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+      <a href={"/ibufuliiru"} class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
       Ibufuliiru
-      </Link>
-      <Link href={"/ibufuliiru"} class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+      </a>
+      <a href={"/ibufuliiru"} class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
       Bafuliiru
-      </Link>
-      <Link href={"/ibufuliiru"} class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+      </a>
+      <a href={"/ibufuliiru"} class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
       Kifuliiru
-      </Link>
-      <Link href={"/ibufuliiru"} class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+      </a>
+      <a href={"/ibufuliiru"} class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
       Guno mukolwa
-      </Link>
-      <Link href={"/ibufuliiru"} class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+      </a>
+      <a href={"/ibufuliiru"} class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
       Tuyandikire
-      </Link>
+      </a>
            
     </div>
     <div>
-      <Link href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Gindi Myazi</Link>
+      <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Gindi Myazi</a>
     </div>
   </div>
 </nav>
