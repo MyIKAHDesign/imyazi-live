329d21ca64b11a936e392d5f2fe52aa179e6b69c
MyIKAHDesign <99662370+MyIKAHDesign@users.noreply.github.com>
2022-10-15 13:17:01 +0200

    I add some spacing between the utton and the other components

--- pages/index.js
+++ pages/index.js
@@ -175,11 +175,13 @@ export default function Home({ subscription }) {
                        <div className="text-right">
                          <TimeAgo date={post._firstPublishedAt} />
                        </div>
+                       </br>
 
                        <button class="bg-grey-light hover:bg-grey text-grey-darkest font-bold py-2 px-4 rounded inline-flex items-center">
   <svg class="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
   <span>Longa guno mwazi</span>
 </button>
+</br>
 
                      </div> 
                       }
