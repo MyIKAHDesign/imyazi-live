c15c5e94d0164a0601f9ae10ba669fd324a3b10d
MyIKAHDesign <99662370+MyIKAHDesign@users.noreply.github.com>
2022-10-15 14:09:22 +0200

    Changing link of the button on the blog posts

--- pages/index.js
+++ pages/index.js
@@ -182,7 +182,7 @@ export default function Home({ subscription }) {
                       {
                         <div>
                           <a
-                            href="/imyazi/umwazi1"
+                            href="/imyazi/mwazi1"
                             class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
                           >
                             Soma bingi
