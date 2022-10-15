3ee55eaa49f744dc91785d01e0bcd28fc0874c89
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2022-10-15 11:22:51 +0200

    Changes the information displayed by page

--- pages/twehe.js
+++ pages/twehe.js
@@ -4,7 +4,52 @@ export default function Twehe(){
     return (
         <div>
             <Navbar/>
-            <title>Twehe</title>
+            <div text-gray-700 body-font py-12 bg-gray-100 px-10>
+        <Head>
+          <title>
+            Tulonge imyazi ye Kifuliiru, Bafuliiru, Ibufuliiru na hoshi na
+            byooshi ikiri miduutu miduutu mu Kifuliiru
+          </title>
+          <link rel="icon" href="/favicon.ico" />
+          <meta
+            name="description"
+            content="Tulonge imyazi ye Kifuliiru, Bafuliiru, Ibufuliiru na hoshi na byooshi ikiri miduutu miduutu mu Kifuliiru"
+          />
+          <meta
+            name="description"
+            content="Kifuliiru. Tulonge imyazi ye Kifuliiru, Bafuliiru, Ibufuliiru na hoshi na byooshi ikiri miduutu miduutu mu Kifuliiru"
+          />
+          <meta
+            name="description"
+            content="Ibufuliiru. Tulonge imyazi ye Bufuliiru, Bafuliiru, na hoshi na byooshi ikiri miduutu miduutu mu Kifuliiru"
+          />
+          <meta
+            name="description"
+            content="Congo. Tulonge imyazi ye Congo, Bafuliiru, na hoshi na byooshi ikiri miduutu miduutu mu Kifuliiru"
+          />
+          <meta
+            name="description"
+            content="Twehe. Longa imyazi mingi ku guno mukolwa. Tuliri Bafuliiru, tolozizi tuhise hala indeto yitu Kifuliiru."
+          />
+          <meta
+            name="description"
+            content="Bafuliiru. Tulonge imyazi ye Bufuliiru, Bafuliiru, na hoshi na byooshi ikiri miduutu miduutu mu Kifuliiru"
+          />
+          <meta
+            name="description"
+            content="Imikolwa yitu. Tulonge imyazi ye mikolwa ya'Bafuliiru mu Kifuliiru namu zindi ndeto zooshi. Imikolwa kwoshi kwo yangaba iliri. Tuyegerere tumenye bingi. Konbwa bweneene"
+          />
+        </Head>
+
+        <div className="max-w-screen-sm mx-auto text-center">
+ 
+        <p className="text-base leading-6 text-indigo-600 font-semibold tracking-wide uppercase">
+        Tulonge imyazi ye Kifuliiru, Bafuliiru, Ibufuliiru na hoshi na byooshi 
+          ikiri miduutu miduutu mu Kifuliiru
+        </p>    
+      </div>
+
+      </div>
         </div>
     );
 }
