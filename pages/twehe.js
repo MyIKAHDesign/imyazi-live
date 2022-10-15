8f3f8996797f1f3198759ae11d2907c8d95ed334
MyIKAHDesign <99662370+MyIKAHDesign@users.noreply.github.com>
2022-10-15 12:27:40 +0200

    I remove the old styling on Twehe page

--- pages/twehe.js
+++ pages/twehe.js
@@ -7,47 +7,47 @@ export default function Twehe() {
   return (
     <div>
       <Navbar />
-      <div text-gray-700 body-font py-12 bg-gray-100 px-10>
-        <Head>
-          <title>
-            Tulonge imyazi ye Kifuliiru, Bafuliiru, Ibufuliiru na hoshi na
-            byooshi ikiri miduutu miduutu mu Kifuliiru
-          </title>
-          <link rel="icon" href="/favicon.ico" />
-          <meta
-            name="description"
-            content="Tulonge imyazi ye Kifuliiru, Bafuliiru, Ibufuliiru na hoshi na byooshi ikiri miduutu miduutu mu Kifuliiru"
-          />
-          <meta
-            name="description"
-            content="Kifuliiru. Tulonge imyazi ye Kifuliiru, Bafuliiru, Ibufuliiru na hoshi na byooshi ikiri miduutu miduutu mu Kifuliiru"
-          />
-          <meta
-            name="description"
-            content="Ibufuliiru. Tulonge imyazi ye Bufuliiru, Bafuliiru, na hoshi na byooshi ikiri miduutu miduutu mu Kifuliiru"
-          />
-          <meta
-            name="description"
-            content="Congo. Tulonge imyazi ye Congo, Bafuliiru, na hoshi na byooshi ikiri miduutu miduutu mu Kifuliiru"
-          />
-          <meta
-            name="description"
-            content="Twehe. Longa imyazi mingi ku guno mukolwa. Tuliri Bafuliiru, tolozizi tuhise hala indeto yitu Kifuliiru."
-          />
-          <meta
-            name="description"
-            content="Bafuliiru. Tulonge imyazi ye Bufuliiru, Bafuliiru, na hoshi na byooshi ikiri miduutu miduutu mu Kifuliiru"
-          />
-          <meta
-            name="description"
-            content="Imikolwa yitu. Tulonge imyazi ye mikolwa ya'Bafuliiru mu Kifuliiru namu zindi ndeto zooshi. Imikolwa kwoshi kwo yangaba iliri. Tuyegerere tumenye bingi. Konbwa bweneene"
-          />
-        </Head>
 
-        <div className="max-w-screen-sm mx-auto text-center">
-          <DefaultContainer/>
-        </div>
+      <Head>
+        <title>
+          Tulonge imyazi ye Kifuliiru, Bafuliiru, Ibufuliiru na hoshi na byooshi
+          ikiri miduutu miduutu mu Kifuliiru
+        </title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Tulonge imyazi ye Kifuliiru, Bafuliiru, Ibufuliiru na hoshi na byooshi ikiri miduutu miduutu mu Kifuliiru"
+        />
+        <meta
+          name="description"
+          content="Kifuliiru. Tulonge imyazi ye Kifuliiru, Bafuliiru, Ibufuliiru na hoshi na byooshi ikiri miduutu miduutu mu Kifuliiru"
+        />
+        <meta
+          name="description"
+          content="Ibufuliiru. Tulonge imyazi ye Bufuliiru, Bafuliiru, na hoshi na byooshi ikiri miduutu miduutu mu Kifuliiru"
+        />
+        <meta
+          name="description"
+          content="Congo. Tulonge imyazi ye Congo, Bafuliiru, na hoshi na byooshi ikiri miduutu miduutu mu Kifuliiru"
+        />
+        <meta
+          name="description"
+          content="Twehe. Longa imyazi mingi ku guno mukolwa. Tuliri Bafuliiru, tolozizi tuhise hala indeto yitu Kifuliiru."
+        />
+        <meta
+          name="description"
+          content="Bafuliiru. Tulonge imyazi ye Bufuliiru, Bafuliiru, na hoshi na byooshi ikiri miduutu miduutu mu Kifuliiru"
+        />
+        <meta
+          name="description"
+          content="Imikolwa yitu. Tulonge imyazi ye mikolwa ya'Bafuliiru mu Kifuliiru namu zindi ndeto zooshi. Imikolwa kwoshi kwo yangaba iliri. Tuyegerere tumenye bingi. Konbwa bweneene"
+        />
+      </Head>
+
+      <div className="max-w-screen-sm mx-auto text-center">
+        <DefaultContainer />
       </div>
+
       <Footer />
     </div>
   );
