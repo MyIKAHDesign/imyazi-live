28ea69c0f4aa299fde150868e10ea93dbcf57a68
MyIKAHDesign <99662370+MyIKAHDesign@users.noreply.github.com>
2022-10-15 13:13:36 +0200

    I added a button with styling

--- pages/index.js
+++ pages/index.js
@@ -176,7 +176,11 @@ export default function Home({ subscription }) {
                          <TimeAgo date={post._firstPublishedAt} />
                        </div>
 
-                       <button>Soma bingi</button>
+                       <button class="bg-grey-light hover:bg-grey text-grey-darkest font-bold py-2 px-4 rounded inline-flex items-center">
+  <svg class="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+  <span>Longa guno mwazi</span>
+</button>
+
                      </div> 
                       }
                     </div>
