7bf5a2f58de0662f180cf4fcb59bde7f6d58f6a5
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2022-10-24 07:44:41 +0200

    Adding a link to the image on posts

--- pages/index.js
+++ pages/index.js
@@ -80,7 +80,7 @@ export default function Home({ subscription }) {
           />
           <meta
             name="description"
-            content="Ibufuliiru. Tulonge imyazi ye Bufuliiru, Bafuliiru, na hoshi na byooshi ikiri miduutu miduutu mu Kifuliiru"
+            content="Ibufuliiru. Bufuliiru. I Bufuliiru. Imwitu Bufuliiru. Imwitu Ibufuliiru. Tulonge imyazi ye Bufuliiru, Bafuliiru, na hoshi na byooshi ikiri miduutu miduutu mu Kifuliiru"
           />
           <meta
             name="description"
@@ -148,11 +148,16 @@ export default function Home({ subscription }) {
                   <div>
                     <div className="shadow-xl rounded-lg overflow-hidden bg-white">
                       {post.photos.map((photo) => (
+                        <a
+                        href="/imyazi/mwazi1"
+                        class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+                      >
                         <Image
                           key={photo.responsiveImage.src}
                           className="w-full"
                           data={photo.responsiveImage}
                         />
+                        </a>
                       ))}
                       {post.content && (
                         <div className="p-4 md:p-8 md:text-xl content">
@@ -179,28 +184,7 @@ export default function Home({ subscription }) {
                           <br />
                         </div>
                       }
-                      {
-                        <div>
-                          <a
-                            href="/imyazi/mwazi1"
-                            class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
-                          >
-                            Soma bingi
-                            <svg
-                              class="w-5 h-5 ml-2 -mr-1"
-                              fill="currentColor"
-                              viewBox="0 0 20 20"
-                              xmlns="http://www.w3.org/2000/svg"
-                            >
-                              <path
-                                fill-rule="evenodd"
-                                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                                clip-rule="evenodd"
-                              ></path>
-                            </svg>
-                          </a>
-                        </div>
-                      }
+                      
                     </div>
                     <div className="mt-4 grid grid-cols-2 text-xs md:text-sm text-gray-500 md:px-8 items-center pb-12">
                       <div className="flex items-center">
