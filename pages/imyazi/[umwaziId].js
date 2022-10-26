7510e6259ef44f66a7763b8516c4e93627cf458f
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2022-10-26 19:26:35 +0200

    Ngweti ngabika i dynamic ID ku myazi

--- pages/imyazi/[umwaziId].js
+++ pages/imyazi/[umwaziId].js
@@ -1,8 +1,13 @@
+import {useRouter} from 'next/router'
+
 export default function Umwazi(){
+    const router = useRouter;
+    const umwaziId = router.querry.umwaziId;
+
     return (
         <div>
             <title>Umwazi mu Kifuliiru. Kifuliiru ndeto ye' Bufuliiru</title>
-            <h4>Umwazi.</h4>
+            <h4>Umwazi. {umwaziId}</h4>
         </div>
     );
 }
