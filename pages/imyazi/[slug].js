719ed9ca1f1c5003fc0703fb306ecdd69ed51430
MyIKAHDesign <99662370+MyIKAHDesign@users.noreply.github.com>
2022-11-02 14:18:08 +0200

    Kept only the simple code on the slug page

--- pages/imyazi/[slug].js
+++ pages/imyazi/[slug].js
@@ -1,12 +1,5 @@
-import {useRouter} from 'next/router'
 
 export default function Umwazi(){
-    const router = useRouter;
-    const [items, setItems] = useState([]);
-    const [text, setText] = useState("");
-
-    const umwaziId = router.querry.umwaziId;
-
     return (
         <div>
             <title>Umwazi mu Kifuliiru. Kifuliiru ndeto ye' Bufuliiru</title>
