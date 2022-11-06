9490c072d1af96fcb7fac45275949d15473e9fe4
MyIKAHDesign <99662370+MyIKAHDesign@users.noreply.github.com>
2022-11-06 17:02:11 +0200

    Nayongera ibindi bindu ku page [slug].js

--- pages/imyazi/[slug].js
+++ pages/imyazi/[slug].js
@@ -3,7 +3,7 @@ export default function Umwazi(){
     return (
         <div>
             <title>Umwazi mu Kifuliiru. Kifuliiru ndeto ye' Bufuliiru</title>
-            <h4>Umwazi.</h4>
+            <h4>Umwazi mu Kifuliiru. Kifuliiru ndeto ye' Bufuliiru.</h4>
         </div>
     );
 }
