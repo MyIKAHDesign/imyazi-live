c2e0512a7d698ff2fefc1fc4c5a3e44c67707ac2
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2022-11-25 08:00:15 +0200

    Nahindula ngisi kwo i menu iliri.

--- components/Navbar.js
+++ components/Navbar.js
@@ -18,27 +18,19 @@ const Navbar = () => {
         </div>
         <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
           <div class="text-white lg:flex-grow">
-            <a href={"/"} class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
-              Ndondeezo
-            </a>
-            <a href={"/ibufuliiru"} class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
-              Ibufuliiru
-            </a>
-            <a href={"/bafuliiru"} class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
-              Bafuliiru
-            </a>
-            <a href={"/kifuliiru"} class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
-              Kifuliiru
-            </a>
-            <a href={"/twehe"} class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
-              Guno mukolwa
-            </a>
-            <a href="https://ibufuliiru.editorx.io/ibufuliiru/tuyandikire" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4" target="_blank">
-              Tuyandikire
-            </a>
            
-            <Link class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4" href='/twehe'>Guno mukolwa 2</Link>
-
+          <Link class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4" href='/'>Ndondeezo</Link>
+          <Link class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4" href='/ibufuliiru'>Ibufuliiru</Link>
+          <Link class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4" href='/bafuliiru'>Bafuliiru</Link>
+          <Link class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4" href='/kifuliiru'>Kifuliiru</Link>
+          <Link class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4" href='/kifuliiru'>Kifuliiru</Link>
+          <Link class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4" href='/twehe'>Guno mukolwa</Link>
+          <Link class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4" href='https://ibufuliiru.editorx.io/ibufuliiru/tuyandikire'>Tuyandikire</Link>
+            
+            {/* <a href={"/"} class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
+              Ndondeezo
+            </a> */}
+            
           </div>
           <div>
             <a href="https://ibufuliiru.editorx.io/imyazi/" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0" target="_blank">Gindi Myazi</a>
