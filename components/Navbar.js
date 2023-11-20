69fce9f2fce371efebfd38874183c7dcea827f03
IKAH Design <99662370+MyIKAHDesign@users.noreply.github.com>
2023-11-19 22:08:11 -0800

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -18,7 +18,6 @@ const Navbar = () => {
         </div>
         <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
           <div class="text-white lg:flex-grow">
-          <div>
             <a href="https://ibufuliiru.editorx.io/imyazi/" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0" target="_blank">Gindi Myazi</a>
           </div>
         </div>
